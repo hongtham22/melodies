daac8a32685786a6b84347929c4584c8fa22a55a
ch1ckenD4nce <hththam22@gmail.com>
2024-10-22 08:13:35 +0700

    header st

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -4,10 +4,10 @@ import ScrollContent from "@/components/scrollContent";
 import SongIntroduce from "@/components/songIntroduce";
 import WaitingList from "@/components/waitingList";
 import { ToastProvider } from "@/components/ui/toast";
-import Header from "@/app/layout/header/Header";
 import Footer from "@/app/layout/footer/Footer";
 import ScrollButton from "@/app/layout/scrollButton/ScrollButton";
 import SidebarAdmin from "@/app/layout/sidebar/SidebarAdmin";
+import HeaderAdmin from "@/app/layout/header/HeaderAdmin";
 
 const AdminLayout: React.FC<{ children: React.ReactNode }> = ({
   children,
@@ -18,7 +18,7 @@ const AdminLayout: React.FC<{ children: React.ReactNode }> = ({
       <ScrollContent>
         <div className="ml-[16.66vw]">
           <main className="flex flex-col items-center">
-            <Header />
+            <HeaderAdmin />
             {children}
             <ToastProvider />
             <Footer />
